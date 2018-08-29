165b32724ed2169e2aa1aaa429a329002ca77c6c
Omer Murat Yildirim <omermuratyildirim@gmail.com>
2018-08-29 17:34:00 +0300

    Add http server

---
+++ config.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/BurntSushi/toml"
+	"github.com/kelseyhightower/envconfig"
+)
+
+// Config for application
+type Config struct {
+	// Listen address for HTTP server.
+	ListenAddress string
+	// Give some time to unfinished HTTP requests before shutting down the server (milliseconds).
+	ShutdownTimeout uint
+}
+
+func (c *Config) Read() error {
+	_, err := toml.DecodeFile(*configPath, c)
+	if err != nil {
+		return err
+	}
+	err = envconfig.Process("", c)
+	if err != nil {
+		return err
+	}
+	c.setDefaults()
+	return nil
+}
+
+func (c *Config) setDefaults() {
+	if c.ListenAddress == "" {
+		c.ListenAddress = "127.0.0.1:8080"
+	}
+	if c.ShutdownTimeout == 0 {
+		c.ShutdownTimeout = 5000
+	}
+}
--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/rs/cors"
 )
 
+// Version of application
 var Version string
 
 func init() {
@@ -13,11 +23,55 @@ func init() {
 	}
 }
 
-var version = flag.Bool("version", false, "version")
+var (
+	version    = flag.Bool("version", false, "version")
+	configPath = flag.String("config", "config.toml", "config file path")
+	config     Config
+	server     http.Server
+)
 
 func main() {
 	flag.Parse()
 	if *version {
 		fmt.Println(Version)
 	}
+
+	err := config.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/events", handleEvents)
+	mux.HandleFunc("/users", handleUsers)
+
+	server.Addr = config.ListenAddress
+	server.Handler = cors.Default().Handler(mux)
+
+	err = server.ListenAndServe()
+
+	if err == http.ErrServerClosed {
+		return
+	}
+	log.Fatal(err)
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	<-stop
+
+	shutdownTimeout := time.Duration(config.ShutdownTimeout) * time.Millisecond
+	ctx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+	defer cancel()
+
+	err = server.Shutdown(ctx)
+	if err != nil {
+		log.Fatal("shutdown error:", err)
+
+	}
+}
+
+func handleEvents(w http.ResponseWriter, r *http.Request) {
+}
+
+func handleUsers(w http.ResponseWriter, r *http.Request) {
 }
