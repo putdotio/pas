58d8be092388c62511e4f6e0d71f9aa3f956378c
Cenk Alti <cenkalti@gmail.com>
2019-03-19 15:05:43 +0300

    discard error on hash.Write

--- internal/handler/handler_test.go
+++ internal/handler/handler_test.go
@@ -92,6 +92,6 @@ func TestPostUsers(t *testing.T) {
 
 func generateUserHash(userID, secret string) string {
 	hash := hmac.New(sha256.New, []byte(secret))
-	hash.Write([]byte(userID))
+	_, _ = hash.Write([]byte(userID))
 	return hex.EncodeToString(hash.Sum(nil))
 }
